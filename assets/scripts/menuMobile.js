d5a1f2f0c37ddc2251c656fb8692ac9bc3374a6e
Carlos-Marte <carlosmartedev@hotmail.com>
2022-12-05 20:45:33 -0400

    Cambiado código de abrir y cerrar el menú. Agregado el auto cerrado al pulsar un link

--- assets/scripts/menuMobile.js
+++ assets/scripts/menuMobile.js
@@ -1,14 +1,36 @@
 const menuIcon = document.querySelector(".header__icon");
+const lista = menuIcon.nextElementSibling;
+const links = document.querySelectorAll(".list__link--menu");
 
-menuIcon.addEventListener('click', (e) => {
-    let imgSrc = menuIcon.getAttribute("src");
-    let lista = menuIcon.nextElementSibling;
-
+// Cambiar menú
+const cambiarImg = (imgSrc) => {
     if(imgSrc === "./assets/image/menu.svg") {
         menuIcon.setAttribute("src", "./assets/image/menu-close.svg")
         lista.classList.toggle("list--menu-show");
-    } else {
-        menuIcon.setAttribute("src", "./assets/image/menu.svg")
-        lista.classList.toggle("list--menu-show");
+        return document.body.style.overflowY = "hidden";
     }
+
+    menuIcon.setAttribute("src", "./assets/image/menu.svg")
+    lista.classList.toggle("list--menu-show");
+    document.body.style.overflowY = "scroll";
+    return document.body.style.overflowY = "scroll";
+}
+
+// Cerrar menú al darle click a los links
+const cerrarMenu = () => {
+    let imgSrc = menuIcon.getAttribute("src");
+    
+    cambiarImg(imgSrc);
+}
+
+menuIcon.addEventListener('click', () => {
+    let imgSrc = menuIcon.getAttribute("src");
+
+    cambiarImg(imgSrc);
+});
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        setTimeout(cerrarMenu, 500);
+    });
 });
